4cdeb0fd55b561049af428d22e7c36c26f27d29a
DownLyfter <thoirvine@gmail.com>
2022-09-12 17:30:52 -0600

    change order of operations

--- install.sh
+++ install.sh
@@ -5,6 +5,11 @@ cd ~/GitClones
 echo "Cloning Repositories"
 git clone https://aur.archlinux.org/yay.git 
 git clone https://github.com/DownLyfter/DotFiles.git 
+cd ../DotFiles
+cp pacman.conf /etc/pacman.conf
+mkdir ~/.config	
+cp * -r ~/.config
+cp -r .* ~/.config 
 cd yay 
 yes | makepkg -si
 echo "Installed yay, configurig config files"
@@ -13,11 +18,6 @@ yes |sudo pacman -Syu $(cat package.txt)
 echo "Finished installing pacman packages."
 cd $startdir
 sudo systemctl enable $(cat services.txt)
-cp ../DotFiles
-cp pacman.conf /etc/pacman.conf
-mkdir ~/.config	
-mv * -r ~/.config
-mv .* ~/.config 
 yes | yay -S shell-color- scripts 
 sudo wget -O /etc/bash.command-not-found https://gitlab.com/dwt1/bash-insulter/-/raw/master/src/bash.command-not-found
 echo "Removing orphans."
